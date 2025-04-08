a03b15058cc4b0442f45d3b0712f4c2b17cc4091
Ana Maria Guzman <anamariagmz31.10@gmail.com>
2025-04-07 22:23:18 -0500

    Add files via upload

--- Tablas.js
+++ Tablas.js
@@ -1,55 +1,63 @@
-// Tabla de verdad del operador && (AND)
-console.log("Tabla de verdad del operador && (AND)");
-console.log("| X | Y | X && Y |");
-console.log("| T | T |   T    |");
-console.log("| T | F |   F    |");
-console.log("| F | T |   F    |");
-console.log("| F | F |   F    |");
+// Tabla de verdad de "Verdadero"
+console.log("Tabla de verdad de 'Verdadero'");
+console.log("| Valor |");
+console.log("|  V    |");
+console.log("|  V    |");
+console.log();
 
-// Tabla de verdad del operador || (OR)
-console.log("\nTabla de verdad del operador || (OR)");
-console.log("| X | Y | X || Y |");
-console.log("| T | T |   T    |");
-console.log("| T | F |   T    |");
-console.log("| F | T |   T    |");
-console.log("| F | F |   F    |");
+// Tabla de verdad de "Falso"
+console.log("\nTabla de verdad de 'Falso'");
+console.log("| Valor |");
+console.log("|  F    |");
+console.log("|  F    |");
+console.log();
 
-// Condicional
-let X = true; 
-let Y = true;
+// Tabla de verdad de una variable
+console.log("\nTabla de verdad de una 'Variable'");
+console.log("| X |");
+console.log("| T |");
+console.log("| F |");
+console.log();
 
-if (X == Y) {
-    console.log("X es igual a Y");
-}
-
-// Tabla de verdad del operador && (AND)
-console.log("Tabla de verdad del operador && (AND)");
-console.log("| X | Y | X && Y |");
-console.log("| T | T |   T    |");
-console.log("| T | F |   F    |");
-console.log("| F | T |   F    |");
-console.log("| F | F |   F    |");
-
-// Tabla de verdad del operador || (OR)
-console.log("\nTabla de verdad del operador || (OR)");
-console.log("| X | Y | X || Y |");
-console.log("| T | T |   T    |");
-console.log("| T | F |   T    |");
-console.log("| F | T |   T    |");
-console.log("| F | F |   F    |");
-
-// Tabla de verdad del operador ! (NOT)
-console.log("\nTabla de verdad del operador ! (NOT)");
+// Tabla de verdad de la negación (¬X)
+console.log("\nTabla de verdad del operador ! (Negación)");
 console.log("| X | !X |");
 console.log("| T |  F |");
 console.log("| F |  T |");
+console.log();
 
-// Condicional de ejemplo
-let X = true; 
-let Y = false;
+// Tabla de verdad de la conjunción (X ∧ Y)
+console.log("\nTabla de verdad del operador && (Conjunción)");
+console.log("| X | Y | X && Y |");
+console.log("| T | T |   T    |");
+console.log("| T | F |   F    |");
+console.log("| F | T |   F    |");
+console.log("| F | F |   F    |");
+console.log();
 
-if (X == Y) {
-    console.log("X es igual a Y");
-} else {
-    console.log("X no es igual a Y");
-}
+// Tabla de verdad de la disyunción (X ∨ Y)
+console.log("\nTabla de verdad del operador || (Disyunción)");
+console.log("| X | Y | X || Y |");
+console.log("| T | T |   T    |");
+console.log("| T | F |   T    |");
+console.log("| F | T |   T    |");
+console.log("| F | F |   F    |");
+console.log();
+
+// Tabla de verdad del condicional (X ⇒ Y)
+console.log("\nTabla de verdad del operador => (Condicional)");
+console.log("| X | Y | X => Y |");
+console.log("| T | T |   T    |");
+console.log("| T | F |   F    |");
+console.log("| F | T |   T    |");
+console.log("| F | F |   T    |");
+console.log();
+
+// Tabla de verdad del bicondicional (X ⇔ Y)
+console.log("\nTabla de verdad del operador ⇔ (Bicondicional)");
+console.log("| X | Y | X ⇔ Y |");
+console.log("| T | T |   T   |");
+console.log("| T | F |   F   |");
+console.log("| F | T |   F   |");
+console.log("| F | F |   T   |");
+console.log();
